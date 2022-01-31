b0a8bcc685d8fa16c1b6669ee6f3767232700182
mporubovic <mawej1@gmail.com>
2022-01-31 03:11:37 +0000

    Fixes #1

--- frontend/src/footer/Footer.js
+++ frontend/src/footer/Footer.js
@@ -2,41 +2,41 @@ import './Footer.css';
 import { Button } from '@mantine/core';
 import React from 'react';
 
-var twitterLogo = require('./twitter-logo.svg');
-var linkedinLogo = require('./linkedin-logo.svg');
-var discordLogo = require('./discord-logo.svg');
+import twitterLogo from './twitter-logo.svg';
+import linkedinLogo from './linkedin-logo.svg';
+import discordLogo from './discord-logo.svg';
 
 class Footer extends React.Component {
     render() {
         return (
             <div className="Footer">
                 <Button>This is the footer</Button>
-                {/* <div class="social" id="social-box">
-                    <div class="twitter" id="twitter-box">
-                        <div class="icon" id="twitter-icon">
-                            {twitterLogo}
-                        </div>
-                        <div class="username" id="twitter-username">
+                <div className="social" id="social-box">
+                    <div className="twitter" id="twitter-box">
+                        <img src={twitterLogo} className="icon" id="twitter-icon">
+                            
+                        </img>
+                        <div className="username" id="twitter-username">
 
                         </div>
                     </div>
-                    <div class="linkedin" id="linkedin-box">
-                        <div class="icon" id="linkedin-icon">
-                            {linkedinLogo}
-                        </div>
-                        <div class="username" id="linkedin-username">
+                    <div className="linkedin" id="linkedin-box">
+                        <img src={linkedinLogo} className="icon" id="linkedin-icon">
+                            
+                        </img>
+                        <div className="username" id="linkedin-username">
 
                         </div>
                     </div>
-                    <div class="discord" id="discord-box">
-                        <div class="icon" id="discord-icon">
-                            {discordLogo}
-                        </div>
-                        <div class="username" id="discord-server">
+                    <div className="discord" id="discord-box">
+                        <img src={discordLogo} className="icon" id="discord-icon">
+                            
+                        </img>
+                        <div className="username" id="discord-server">
 
                         </div>
                     </div>
-                </div> */}
+                </div>
             </div>
         );
     };
