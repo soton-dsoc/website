2c2aaef87cd3b3fa6ec1fcba18ec396b0a56be3e
German Nikolishin <german.nikolishin@gmail.com>
2022-01-31 13:14:50 +0000

    basic setup for event modal

---
+++ frontend/src/modal/event/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const TitleWrapper = styled.div`
+    padding: 1rem;
+    background: linear-gradient(90deg, #0066FF, #00D1FF);
+    border-radius: 50px;
+    padding: 0.5rem 3rem;
+`
+
+export const ModalWrapper = styled.div`
+    background-color: #5E5E5E;
+    padding: 5rem;
+`
