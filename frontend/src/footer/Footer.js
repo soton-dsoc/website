de95a06396dfa3caa0767bc2ac3a7b388cc266bd
viktaur <victorgabal@gmail.com>
2022-01-31 13:48:11 +0000

    quite better

--- frontend/src/footer/Footer.js
+++ frontend/src/footer/Footer.js
@@ -10,33 +10,26 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="Footer">
-                <Button>This is the footer</Button>
-                <div className="social" id="social-box">
-                    <div className="twitter" id="twitter-box">
+                <div id="social-box">
+                    <div className="social twitter" id="twitter-box">
                         <img src={twitterLogo} className="icon" id="twitter-icon">
                             
                         </img>
-                        <div className="username" id="twitter-username">
-
-                        </div>
                     </div>
-                    <div className="linkedin" id="linkedin-box">
+                    <div className="social linkedin" id="linkedin-box">
                         <img src={linkedinLogo} className="icon" id="linkedin-icon">
                             
                         </img>
-                        <div className="username" id="linkedin-username">
-
-                        </div>
                     </div>
-                    <div className="discord" id="discord-box">
+                    <div className="social discord" id="discord-box">
                         <img src={discordLogo} className="icon" id="discord-icon">
                             
                         </img>
-                        <div className="username" id="discord-server">
-
-                        </div>
                     </div>
                 </div>
+                <div class="menutext" id="home">Home</div>
+                <div class="menutext" id="home">About</div>
+                <div class="menutext" id="home">Events</div>
             </div>
         );
     };
