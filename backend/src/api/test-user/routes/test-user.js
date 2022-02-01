50c15bcfc53c7637e3443a9daaca311d37fc0101
German Nikolishin <german.nikolishin@gmail.com>
2022-02-01 16:46:30 +0000

    remove garbage

--- backend/src/api/test-user/routes/test-user.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test-user router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::test-user.test-user');
