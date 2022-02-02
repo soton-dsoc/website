ba6db8b60cafc1bed6017ba1862c2b95d0bb0096
German Nikolishin <german.nikolishin@gmail.com>
2022-02-02 21:21:51 +0000

    email and docker

--- backend/src/api/form/controllers/form.js
+++ backend/src/api/form/controllers/form.js
@@ -23,13 +23,20 @@ module.exports = createCoreController('api::form.form', ({ strapi }) => ({
             let date = new Date(event.startDatetime).toLocaleString('en-GB', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
             let time = new Date(event.startDatetime).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit'});
             console.log(date, time);
-            //TODO: uncomment when html template is ready
-            // await strapi.plugins['email'].services.email.send({
-            //     to: entry.email, // Change to your recipient
-            //     from: 'no-reply@soton-dsoc.org', // Change to your verified sender
-            //     subject: event.title + ' confirmation',
-            //     html: `<p>We look forward seeing you in ${event.location} on ${date} at ${time}</p>`,
-            // });
+            await strapi.plugins['email'].services.email.send({
+                to: entry.email, // Change to your recipient
+                from: 'no-reply@soton-dsoc.org', // Change to your verified sender
+                subject: event.title + ' confirmation',
+                html: 
+                `<h3>Hey ${entry.name}!</h3>` +
+                `<div>` +
+                `<p>Thank you for signing up for <strong>${event.title}</strong>!</p>` +
+                `<p>We look forward seeing you in <strong>${event.location}</strong> on <strong>${date}</strong> at <strong>${time}</strong></p>` +
+                `<br><br>` +
+                `<p>Keep posted for other events on the website https://soton-dsoc.org and social media</p>` +
+                `<img src="https://github.com/soton-dsoc/brand-assets/raw/main/logos/PNG/soton-dSoc-full-black.png" width="100">` +
+                `</div>`,
+            });
             console.log('email sent');
             ctx.response.status = 201;
             ctx.response.message = "Response has been saved, email has been sent.";
