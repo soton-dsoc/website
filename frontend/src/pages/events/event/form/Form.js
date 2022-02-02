6251048cd2f9cd1a8d8853df111780464de4f5fe
mporubovic <mawej1@gmail.com>
2022-02-02 23:55:33 +0000

    hide form after registration

--- frontend/src/pages/events/event/form/Form.js
+++ frontend/src/pages/events/event/form/Form.js
@@ -7,8 +7,6 @@ import validator from 'validator'
 
 function Form(props) {
 
-    console.log(props)
-
     const [studyAreas, setStudyArea] = useState(['Computer Science', 'Electronics', 'Business', 'Economics', 'Law'])
 
     const [chips, setChips] = useState([])
@@ -24,6 +22,8 @@ function Form(props) {
     const [formSourceError, setFormSourceError] = useState(false)
     const [formSchoolError, setFormSchoolError] = useState(false)
     const [formExperienceError, setFormExperienceError] = useState(false)
+
+    const [formSubmitted, setFormSubmitted] = useState(false)
 
     const register = () => {
 
@@ -71,7 +71,9 @@ function Form(props) {
 
 
         axios.post(configs.api.baseUrl + '/api/forms', payload).then(r => {
-            console.log(r)
+            if (r.status == "201") {
+                setFormSubmitted(true)
+            }
         })
 
     }
@@ -79,86 +81,93 @@ function Form(props) {
 
     return (
         <div className='Form'>
-            <TextInput className='form-input'
-                ref={formName}
-                error={formNameError}
-                onChange={() => setFormNameError(false)}
-                required
-                label="Your name"
-                placeholder="Tim Berners-Lee"
-            />
+            <div style={ { display: formSubmitted ? 'none' : null } }>
+                <TextInput className='form-input'
+                    ref={formName}
+                    error={formNameError}
+                    onChange={() => setFormNameError(false)}
+                    required
+                    label="Your name"
+                    placeholder="Tim Berners-Lee"
+                />
 
 
-            <TextInput className='form-input'
-                ref={formEmail}
-                required
-                error={formEmailError}
-                onChange={() => setFormEmailError(false)}
-                label="Your university email"
-                placeholder="mp3u21@soton.ac.uk"
-            />
+                <TextInput className='form-input'
+                    ref={formEmail}
+                    required
+                    error={formEmailError}
+                    onChange={() => setFormEmailError(false)}
+                    label="Your university email"
+                    placeholder="mp3u21@soton.ac.uk"
+                />
 
 
-            <Select className='form-input'
-                ref={formSource}
-                error={formSourceError}
-                onChange={() => setFormSourceError(false)}
-                required
-                label="How did you hear about this event?"
-                placeholder="Pick one"
-                data={['Poster', 'Email', 'Social media', 'Word of mouth']}
-            />
+                <Select className='form-input'
+                    ref={formSource}
+                    error={formSourceError}
+                    onChange={() => setFormSourceError(false)}
+                    required
+                    label="How did you hear about this event?"
+                    placeholder="Pick one"
+                    data={['Poster', 'Email', 'Social media', 'Word of mouth']}
+                />
 
 
-            <Select className='form-input'
-                ref={formSchool}
-                error={formSchoolError}
-                onChange={() => setFormSchoolError(false)}
-                required
-                label="What is your field of study?"
-                placeholder="Pick one or type"
-                description="If your area of study is not listed, type it in"
-                searchable
-                creatable
-                getCreateLabel={(query) => `${query}`}
-                onCreate={(query) => setStudyArea((studyAreas) => [...studyAreas, query])}
-                data={studyAreas}
-            />
+                <Select className='form-input'
+                    ref={formSchool}
+                    error={formSchoolError}
+                    onChange={() => setFormSchoolError(false)}
+                    required
+                    label="What is your field of study?"
+                    placeholder="Pick one or type"
+                    description="If your area of study is not listed, type it in"
+                    searchable
+                    creatable
+                    getCreateLabel={(query) => `${query}`}
+                    onCreate={(query) => setStudyArea((studyAreas) => [...studyAreas, query])}
+                    data={studyAreas}
+                />
 
-            <Select className='form-input'
-                ref={formExperience}
-                error={formExperienceError}
-                onChange={() => setFormExperienceError(false)}
-                required
-                label="What is your experience with the topic?"
-                placeholder="Pick one"
-                data={['Not familiar', 'Somewhat familiar', 'Very familiar']}
-            />
+                <Select className='form-input'
+                    ref={formExperience}
+                    error={formExperienceError}
+                    onChange={() => setFormExperienceError(false)}
+                    required
+                    label="What is your experience with the topic?"
+                    placeholder="Pick one"
+                    data={['Not familiar', 'Somewhat familiar', 'Very familiar']}
+                />
 
-            {
-                props.questions !== null && props.questions.map((q, i) =>
-                    <InputWrapper
-                        label={q.text}
-                        description={q.description}
-                        key={i}
-                    >
-                        {
-                            q.type == "Chips" &&
-                            <Chips multiple style={{ fontWeight: 400 }} onChange={setChips}>
+                {
+                    props.questions !== null && props.questions.map((q, i) =>
+                        <InputWrapper
+                            label={q.text}
+                            description={q.description}
+                            key={i}
+                        >
+                            {
+                                q.type == "Chips" &&
+                                <Chips multiple style={{ fontWeight: 400 }} onChange={setChips}>
 
-                                {
-                                    q.options.map((o, j) =>
-                                        <Chip value={o} key={j}>{o}</Chip>
-                                    )
-                                }
-                            </Chips>
-                        }
-                    </InputWrapper>
-                )
-            }
+                                    {
+                                        q.options.map((o, j) =>
+                                            <Chip value={o} key={j}>{o}</Chip>
+                                        )
+                                    }
+                                </Chips>
+                            }
+                        </InputWrapper>
+                    )
+                }
 
-            <div className='event-register'>
-                <div className='event-button' onClick={register}>Register</div>
+                <div className='event-register'>
+                    <div className='event-button' onClick={register}>Register</div>
+                </div>
+            </div>
+
+
+            <div style={ { display: formSubmitted ? null : 'none' } }>
+                Thank you for registering - you will receive a confirmation email shortly
             </div>
         </div>
     )
