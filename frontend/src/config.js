b240bc4a2b6ce3a8c936f270be1066bc6497b9c0
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2022-02-02 11:39:36 +0000

    Added default config links

---
+++ frontend/src/config.js
@@ -0,0 +1,8 @@
+const configs = {
+    discord_invite: "https://discord.gg/3efMw45QMN",
+    linkedin: "https://www.linkedin.com/company/dsoc",
+    twitter: "https://twitter.com/SotonDsoc",
+    github: "https://github.com/soton-dsoc",
+}
+
+export default configs;
