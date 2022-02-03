50a57a7f9f3f254418786836f92abf10044231c6
German Nikolishin <german.nikolishin@gmail.com>
2022-02-03 19:49:50 +0000

    fix production built

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', "http://127.0.0.1")
+  url: env('URL', "http://127.0.0.1"),
+  admin: {
+    url: env('URL', "http://127.0.0.1") + "/admin",
+  }
 });
